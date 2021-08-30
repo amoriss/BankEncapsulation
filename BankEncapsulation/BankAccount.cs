e5ed6a836a49b2aece88d65d1f0f8b768b1f330a
Amoriss <amoriss@protonmail.com>
2021-08-30 11:59:04 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double money)
+        {
+            balance += money;
+            Console.WriteLine($" You have deposited this amount: {money} \n" +
+                $"This is your balance: {balance}");
+        }
+
+        public double GetBalance()
+        {
+            return balance; 
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pin = "1234";
+            BankAccount myAccount = new BankAccount();
+            WelcomeScreen();
+
+              void WelcomeScreen()
+            {
+                Console.WriteLine("BANK OF ALABAMA."); 
+                Console.WriteLine("Enter pin: ");
+                string enteredPin =Console.ReadLine();
+                if (enteredPin == pin)
+
+                {
+                    Console.WriteLine("Do you want to enter money in account? Enter: YES or NO");
+                    var answer = Console.ReadLine();//.ToLower();
+                    switch (answer.ToLower())
+                    {
+                        case "yes":
+                        case "y":
+
+                            Console.WriteLine("Enter in amount you would like to deposit");
+                            var amount = Convert.ToDouble(Console.ReadLine());
+                            myAccount.Deposit(amount);
+                            break;
+
+                        case "no":
+                        case "n":
+                            Console.WriteLine("Your total balance is: ");
+                            Console.WriteLine(myAccount.GetBalance());
+                            break;
+
+                        default:
+                            Console.WriteLine("You have exited application");
+                            break;
+                    }
+                }
+                else if (pin != enteredPin)
+                {
+                    Console.WriteLine("Incorrect Pin Number. Try again");
+                    WelcomeScreen();
+                }
+                    
+                else
+                    Console.WriteLine("You have exited program");
+
+            }
+             
+        }
+    }
+}
